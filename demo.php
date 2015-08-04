e347c4ea476e2f2df12c239a9859975a2724ec55
Andrey Harkusha <xfeaton@gmail.com>
2015-08-05 01:48:48 +0300

    fix name inc file

--- demo.php
+++ demo.php
@@ -1,9 +1,20 @@
 <?php
 
-require 'class.Adress.inc';
+require 'class.Address.inc';
 
 echo "<h2>Instantiating Address</h2>";
 $address = new Address;
 
 echo "<h2>Empty Address</h2>";
 echo "<tt><pre>". var_export($address, TRUE)."</pre></tt>";
+
+echo "<h2>Setting Properties...</h2>";
+$address->street_name1 = '555 Fake Street';
+$address->city_name = 'Townsville';
+$address->subdivision_name = 'State';
+$address->postal_code = '12345';
+$address->country_name = 'United States of America';
+echo "<tt><pre>". var_export($address, TRUE)."</pre></tt>"; 
+
+echo '<h2>Display address</h2>';
+$address->display();
