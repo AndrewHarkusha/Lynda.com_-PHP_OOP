0e343f1ae837eb78a814244cd9c6eb4c04d4dc11
Andrey Harkusha <xfeaton@gmail.com>
2015-08-05 01:34:05 +0300

    add demo

---
+++ demo.php
@@ -0,0 +1,9 @@
+<?php
+
+require 'class.Adress.inc';
+
+echo "<h2>Instantiating Address</h2>";
+$address = new Address;
+
+echo "<h2>Empty Address</h2>";
+echo "<tt><pre>". var_export($address, TRUE)."</pre></tt>";
